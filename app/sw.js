2164b6eb82da8d6e92703cb232d453e7a4999e3d
longchanp7-hub <longchanp7@gmail.com>
2026-07-15 18:23:26 +0900

    Add service worker

---
+++ app/sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'sushi-fair-v2';
+const STATIC = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.pathname.endsWith('/data/fairs.json')) {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const clone = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, clone));
+    return response;
+  })));
+});
